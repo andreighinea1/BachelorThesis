f115796c01b8293d8c242084c5c759df73b540f2
aghinea <aghinea@bitdefender.com>
2024-02-10 21:34:29 +0200

    hm

--- model/utils/update_git.sh
+++ model/utils/update_git.sh
@@ -8,6 +8,6 @@ git pull
 
 # Install packages directly into the py312 environment
 #conda install --yes --name py312 -c conda-forge --file requirements.txt
-
-source ~/anaconda3/bin/activate ~/anaconda3/envs/py312
+conda activate py312
+#source ~/anaconda3/bin/activate ~/anaconda3/envs/py312
 pip install -r requirements.txt
