b6ff20650b97174e286d9869032ce5f60557e43d
aghinea <aghinea@bitdefender.com>
2024-06-05 21:51:14 +0300

    Fixed EEGDataset to use verdict_to_index

--- model/dataset_processing/eeg_dataset.py
+++ model/dataset_processing/eeg_dataset.py
@@ -4,6 +4,9 @@ from torch.utils.data import Dataset
 class EEGDataset(Dataset):
     def __init__(self, dataframe):
         self.dataframe = dataframe
+        self.unique_verdicts = sorted(dataframe['Verdict'].unique())
+        self.verdict_to_index = {int(verdict): idx for idx, verdict in enumerate(self.unique_verdicts)}
+        self.index_to_verdict = {idx: int(verdict) for verdict, idx in self.verdict_to_index.items()}
 
     def __len__(self):
         return len(self.dataframe)
@@ -15,6 +18,12 @@ class EEGDataset(Dataset):
         xT_augmented = row['EEG_Time_Augmented'].clone().detach()
         xF = row['EEG_Frequency'].clone().detach()
         xF_augmented = row['EEG_Frequency_Augmented'].clone().detach()
-        y = row['Verdict'].clone().detach()
+        y_idx = self.verdict_to_index[row['Verdict']]
 
-        return xT, xT_augmented, xF, xF_augmented, y
+        return xT, xT_augmented, xF, xF_augmented, y_idx
+
+    def get_class_index(self, verdict):
+        return self.verdict_to_index[verdict]
+
+    def get_verdict(self, index):
+        return self.index_to_verdict[index]
